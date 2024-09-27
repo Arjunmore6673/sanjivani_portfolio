f999277be4fee28e33cf0318c92bacc30fb0f8ad
Arjun more <amore@chirpn.com>
2024-09-28 01:41:45 +0530

    fix theme

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import LandingPage from './pages/landing'
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Card3d from "./components/Card3d";
-import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core/styles';
-const theme = createTheme();
-const useStyles = makeStyles((theme) => {
-    root: {
-        // some CSS that accesses the theme
-    }
-});
+import { ThemeProvider } from '@emotion/react';
 
+const theme = {
+    palette: {
+        primary: 'green',
+        text: '#fff',
+    },
+};
 
 
 
 function App() {
-    const classes = useStyles(); // ❌ If you have this, consider moving it
     return <ThemeProvider theme={theme}>
-
-
         <BrowserRouter>
             <Routes>
                 <Route path="/card3d" element={<Card3d />} />
